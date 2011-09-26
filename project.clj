3e22c4c33d79c6d4f7ac401e8445933f143750bf
Max Penet <max.penet@gmail.com>
2011-09-26 10:40:31 +0200

    sync

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject clojure-hbase "0.90.0"
+(defproject com.compasslabs/clojure-hbase-schemas "0.90.4"
   :description "A convenient Clojure interface to HBase."
+  :license "See LICENSE file"
+  :url "http://github.com/compasslabs/clojure-hbase-schemas"
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-		 [org.apache.hbase/hbase "0.90.0"]
+		 [org.apache.hbase/hbase "0.90.4"]
 		 [org.apache/zookeeper "3.3.2"]
-		 [clj-time "0.2.0-SNAPSHOT"]
+		 [clj-time "0.3.0"]
 		 [clj-serializer "0.1.1"]
                  [clj-json "0.3.1"]
 		 [log4j/log4j "1.2.15" :exclusions [javax.mail/mail
@@ -13,9 +15,5 @@
 						    com.sun.jdmk/jmxtools
 						    com.sun.jmx/jmxri]]
 		 [commons-logging/commons-logging "1.0.4"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.3.0-SNAPSHOT"]]
-;;  :resources-path "./"
-  :repositories {"compass" "http://build.compasslabs.com/maven/content/groups/all"})
-
-(ns leiningen.docs)
-;(defn docs...)
+  :dev-dependencies [[swank-clojure/swank-clojure "1.3.0-SNAPSHOT"]
+		     [lein-clojars "0.7.0"]])
